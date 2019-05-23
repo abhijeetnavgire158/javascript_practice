aa0321bc889424c59fec22b4e695315aeeae633d
Abhijeet Navgire <abhijeet.navgire@cuelogic.com>
2019-05-23 16:12:03 +0530

    string functions

---
+++ string.js
@@ -0,0 +1,16 @@
+var str1 = "Abhijeet";
+var str2 = new String("Abhijeet ");
+
+console.log(typeof str1);
+console.log(typeof str2);
+
+console.log(str1.charAt(3));
+console.log(str1.charCodeAt(3));
+console.log(str1.concat(' Navgire'));
+console.log(str1.endsWith('eet'));
+console.log(str1.indexOf('e'));
+console.log(str1.lastIndexOf('ea'));
+console.log(str1.repeat(4));
+console.log(str1.replace('ee','i'));
+console.log(str1.substr(4,4));
+console.log(str1.substring(4,8));
